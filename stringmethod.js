8dc7255850de2aa8076596e04b6d38b7aada3366
Bhavnameshram <bhawanameshram1910@gmail.com>
2025-10-11 14:20:12 +0530

    stringmethod

---
+++ stringmethod.js
@@ -0,0 +1,24 @@
+// length
+let bhavna = "matterhogya";
+console.log(bhavna.length);
+   
+// charater
+let harshda = "somewhathappen";
+console.log(harshda.charAt(2));
+
+// chaCodeAt
+let pratik = "lethowareyou";
+console.log(pratik.charCodeAt(5));
+
+// chartpointcode
+let gaurav = "anotherfind";
+console.log(gaurav.codePointAt(5));
+ 
+// at
+let aayush = "bravethegranter";
+console.log(aayush.at(6));
+
+// repeat
+let neha ="may the window";
+console.log(neha.repeat(5));
+ 
