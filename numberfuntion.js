cef56c5460bda572c5ca7df42d3f53fe35af8fdb
Bhavnameshram <bhawanameshram1910@gmail.com>
2025-10-14 15:13:27 +0530

    valueoff

--- numberfuntion.js
+++ numberfuntion.js
@@ -7,15 +7,19 @@ console.log(a.toString(16));
 
 // toExponentional
 var b= 56778.746576;
-console.log(a.toExponential()); 
-console.log(a.toExponential(3)); 
-console.log(a.toExponential(5)); 
+console.log(b.toExponential()); 
+console.log(b.toExponential(3)); 
+console.log(b.toExponential(5)); 
 
 // toFixed
 var c= 5767.677;
-console.log(a.toFixed(2)); 
+console.log(c.toFixed(2)); 
  
 
 // toPrecision
-var d= 5465.4564;
-console.log(a.toPrecision(2)); 
+var d= 5465.4564
+console.log(d.toPrecision(4)); 
+
+// valueOf()
+var e=455;
+console.log(e.toPrecision()); 
