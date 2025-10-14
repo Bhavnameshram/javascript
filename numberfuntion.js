40bd3d787563ddee2f18265ae49612b6b24333e2
Bhavnameshram <bhawanameshram1910@gmail.com>
2025-10-14 15:05:19 +0530

    to expinati

--- numberfuntion.js
+++ numberfuntion.js
@@ -5,4 +5,17 @@ console.log(a.toString(2));
 console.log(a.toString(8)); 
 console.log(a.toString(16)); 
 
+// toExponentional
+var b= 56778.746576;
+console.log(a.toExponential()); 
+console.log(a.toExponential(3)); 
+console.log(a.toExponential(5)); 
 
+// toFixed
+var c= 5767.677;
+console.log(a.toFixed(2)); 
+ 
+
+// toPrecision
+var d= 5465.4564;
+console.log(a.toPrecision(2)); 
