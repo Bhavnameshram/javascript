a246de81d30e7a6a7d39d0968e469a98a5887ea7
Bhavnameshram <bhawanameshram1910@gmail.com>
2025-10-18 17:17:55 +0530

    objects

--- function.js
+++ function.js
@@ -1,8 +1,8 @@
-function Haldi (b1,b2)
-{
-     console.log (b1 + "WEDS" + b2)
-}
- Haldi("Ashish","Payal");
+// function Haldi (b1,b2)
+// {
+//      console.log (b1 + "WEDS" + b2)
+// }
+//  Haldi("Ashish","Payal");
  
 
  
