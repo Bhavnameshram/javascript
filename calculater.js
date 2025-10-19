8ac4e69e8ce12f8a1e8c4fd008e5905b2580eeec
Bhavnameshram <bhawanameshram1910@gmail.com>
2025-10-19 12:37:24 +0530

    chnages

--- calculater.js
+++ calculater.js
@@ -1,5 +1,7 @@
 var a = prompt();
+   a= number(a);
 var b = prompt();
+b= number(b);
 function Add(a, b) {
     return a + b;
 }
@@ -12,3 +14,9 @@ Mul(a, b){
 div(a, b){
     return a / b;
 }
+// addition
+// subtraction
+// multiplicatiom
+// division
+let userprompt (" Enter what operation");
+to perform 1=Add , 2= Sub, 3=mul , 4= mul 
