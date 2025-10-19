72ec5b6be132db52c87aa2c0adfd9f247f45c21f
Bhavnameshram <bhawanameshram1910@gmail.com>
2025-10-19 12:32:24 +0530

    changes

---
+++ calculater.js
@@ -0,0 +1,14 @@
+var a = prompt();
+var b = prompt();
+function Add(a, b) {
+    return a + b;
+}
+Sub(a, b){
+    return a - b;
+}
+Mul(a, b){
+    return a * b;
+}
+div(a, b){
+    return a / b;
+}
