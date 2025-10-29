7a29e6e070be1a899443194a4a521475a07d2b87
Bhavnameshram <bhawanameshram1910@gmail.com>
2025-10-29 22:51:12 +0530

    at()

--- arraymthods.js
+++ arraymthods.js
@@ -3,7 +3,12 @@ var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
 let size = cars.length;
 document.write(size);
 
-// toString method
+// toString() method
 var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
 let b = cars.toString();
 document.write(b);
+
+// at() method
+var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
+let c = cars.at(4);
+document.write(c);
