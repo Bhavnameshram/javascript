8b49846f45abb33751e596c945dcf3d3e744e3e1
Bhavnameshram <bhawanameshram1910@gmail.com>
2025-10-29 22:47:03 +0530

    tostring

--- arraymthods.js
+++ arraymthods.js
@@ -2,3 +2,8 @@
 var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
 let size = cars.length;
 document.write(size);
+
+// toString method
+var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
+let b = cars.toString();
+document.write(b);
