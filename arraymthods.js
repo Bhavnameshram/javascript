63c2a58a0e5b0bd5926eff7bd21451ee8be1cde2
Bhavnameshram <bhawanameshram1910@gmail.com>
2025-10-31 13:52:48 +0530

    cahnges

--- arraymthods.js
+++ arraymthods.js
@@ -1,79 +1,79 @@
-// lenght method
-var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
-let size = cars.length;
-document.write(size);
+// // lenght method
+// var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
+// let size = cars.length;
+// document.write(size);
 
-// toString() method
-var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
-let b = cars.toString();
-document.write(b);
+// // toString() method
+// var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
+// let b = cars.toString();
+// document.write(b);
 
-// at() method
-var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
-let c = cars.at(4);
-document.write(c);
+// // at() method
+// var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
+// let c = cars.at(4);
+// document.write(c);
 
-// join() method
-var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
-let d = cars.join("$");
-document.write(d);
+// // join() method
+// var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
+// let d = cars.join("$");
+// document.write(d);
 
-// pop method
-var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
-let e = cars.pop();
-document.write(e);
+// // pop method
+// var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
+// let e = cars.pop();
+// document.write(e);
 
-// push method
-var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
-let f = cars.push("Vegnar");
-document.write(f);
+// // push method
+// var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
+// let f = cars.push("Vegnar");
+// document.write(f);
 
-// shift method
-var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
-let g= cars.shift();
-document.write(g);
+// // shift method
+// var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
+// let g= cars.shift();
+// document.write(g);
 
-// unshift method
-var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
-let h= cars.unshift("Kia");
-document.write(h);
+// // unshift method
+// var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
+// let h= cars.unshift("Kia");
+// document.write(h);
 
-// delete method
-var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
-let i= delete cars[3];
-document.write(i);
+// // delete method
+// var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
+// let i= delete cars[3];
+// document.write(i);
 
-// concat method
-var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
-let j= cars.concat();
-document.write(j);
+// // concat method
+// var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
+// let j= cars.concat();
+// document.write(j);
 
-// copywithin method
-var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
-let k= cars.copyWithin(2,4);
-document.write(k);
+// // copywithin method
+// var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
+// let k= cars.copyWithin(2,4);
+// document.write(k);
 
-// flat method
-var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
-let l= cars.flat();
-document.write(l);
+// // flat method
+// var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
+// let l= cars.flat();
+// document.write(l);
 
-// slice method
-var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
-let m = cars.slice(1);
-document.write(m);
+// // slice method
+// var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
+// let m = cars.slice(1);
+// document.write(m);
 
 // slice method
 var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
 let n = cars.slice(1,3);
 document.write(n);
 
-// splice method
-var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
-let o = cars.splice(2,0, "Wagnar", "Shift");
-document.write(o);
+// // splice method
+// var cars=["Volvo","Hariear","BMW","Creata","Defender"];
+// cars.splice(2,0, "Wagnar", "Shift");
+// document.write(cars);
 
-// tospliced
-var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
-let p = cars.toSpliced(2,0,);
-document.write(o);
+// // tospliced
+// var cars=["Volvo", "Hariear","BMW","Creata","Defender"];
+// let p = cars.toSpliced(0,1);
+// document.write(p);
